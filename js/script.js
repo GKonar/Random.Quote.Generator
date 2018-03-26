cc3aa0827b1d66b3fb8513541ad89a6a0de9a22f
Grzegorz Konarski <grzegorz.konarski86@gmail.com>
2018-03-26 16:09:36 +0200

    Test function

--- js/script.js
+++ js/script.js
@@ -9,12 +9,28 @@ function createTweet(input) {
     var data = input[0];
 
     var quoteText = $(data.content).text().trim();
-     console.log(quoteText);
     var quoteAuthor = data.title;
-
-    if (!quoteAuthor.length) {
+	
+	if (!quoteAuthor.length) {
         quoteAuthor = "Unknown author";
     }
 
+    var tweetText = "Quote of the day - " + quoteText + " Author: " + quoteAuthor;
+    
+    if (tweetText.length > 140) {
+    getQuote();
+	}	else {
+    var tweet = tweetLink + encodeURIComponent(tweetText);
+    $('.quote').text(quoteText);
+    $('.author').text("Author: " + quoteAuthor);
+    $('.tweet').attr('href', tweet);
+		}
 }
+
+$(document).ready(function() {
+    getQuote();
+    $('.trigger').click(function() {
+        getQuote();
+    })
+});
 
