c07b79bdecb32d4de2cbe1a4d80ade1ff90c4c77
Grzegorz Konarski <grzegorz.konarski86@gmail.com>
2018-03-26 15:29:06 +0200

    Add Js

---
+++ js/script.js
@@ -0,0 +1,20 @@
+var tweetLink = "https://twitter.com/intent/tweet?text=";
+var quoteUrl = "https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1";
+
+function getQuote() {
+    $.getJSON(quoteUrl, createTweet);
+}
+
+function createTweet(input) {
+    var data = input[0];
+
+    var quoteText = $(data.content).text().trim();
+     console.log(quoteText);
+    var quoteAuthor = data.title;
+
+    if (!quoteAuthor.length) {
+        quoteAuthor = "Unknown author";
+    }
+
+}
+
